5f0ec2c86c95bed3cfa6592460a859cdecb30720
Dipper98 <134005854+Dip98@users.noreply.github.com>
2024-06-12 16:13:36 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -17,3 +17,20 @@ function changePage(index){
 	});
 }
 changePage(0);
+
+function searchPrograms() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("search-bar");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("program-list");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
