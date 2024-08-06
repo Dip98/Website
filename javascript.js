18f91ab7b16a5f5a3da8b2f7d32147f2819baee5
Dipper98 <134005854+Dip98@users.noreply.github.com>
2024-08-05 19:36:33 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -33,3 +33,36 @@ function searchPrograms() {
         }
     }
 }
+function searchShaders() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("shade-search-bar");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("shader-list");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+function searchArticles() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("art-search-bar");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("article-list");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
